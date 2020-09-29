b7269e88443d78e87b1f44e406df599cc68731f2
Gescof <gescolarfernandez@gmail.com>
2020-09-29 10:21:06 +0200

    OnClickListener implemented (new way)

--- app/src/main/java/com/example/seccion_01/MainActivity.java
+++ app/src/main/java/com/example/seccion_01/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn1;
 
     @Override
@@ -16,12 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn1 = findViewById(R.id.button1);
-        btn1.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(this);
+        /*btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_LONG).show();
             }
-        });
+        });*/
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_LONG).show();
     }
 
 }
